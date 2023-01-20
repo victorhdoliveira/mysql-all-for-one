5f80f222fab9d2f2958570a67f9b3a3e94bf6fed
Victor Hugo <victorhdoliveira@gmail.com>
2023-01-20 18:59:31 -0300

    Projeto Finalizado

--- desafio17.sql
+++ desafio17.sql
@@ -1,2 +1,2 @@
-SELECT id, supplier_id FROM northwind.purchase_orders
+SELECT id, supplier_id FROM purchase_orders
     WHERE supplier_id = 1 OR supplier_id = 3  OR supplier_id = 5 OR supplier_id = 7;
